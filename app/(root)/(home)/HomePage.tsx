3b16dcc55137e6c6034866d96641bd81d1efc297
Abin Antony <abina5448@gmail.com>
2024-05-18 10:35:09 +0530

    navbar

--- app/(root)/(home)/HomePage.tsx
+++ app/(root)/(home)/HomePage.tsx
@@ -37,16 +37,15 @@ const HomePage = () => {
             <div ref={section1Ref} className='w-full relative z-[2]'>
                 <BackgroundGradientAnimation className="min-h-[100vh] w-full overflow-x-hidden bg-white/90 flex flex-col items-center justify-between" >
                     <motion.div className="flex flex-col items-center justify-center h-full mt-[9rem]">
-                        <motion.h2 initial={{ opacity: 0, y: 20 }} transition={{ duration: 0.4, stiffness: 1 }} animate={{ opacity: 1, y: 0 }} className="text-[45px] max-w-[800px] text-center font-extrabold title-animated ">Crafting Digital Excellence through WebCodeCreators</motion.h2>
+                        <motion.h2 initial={{ opacity: 0, y: 20 }} transition={{ duration: 0.4, stiffness: 1 }} animate={{ opacity: 1, y: 0 }} className="md:text-[45px] text-[40px] max-w-[800px] text-center font-extrabold title-animated ">
+                            <span className='md:text-[45px] text-[25px]'>Crafting Digital Excellence through</span> <br />WebCodeCreators</motion.h2>
                         <p className="text-primary font-medium">Where Vision Meets Innovation</p>
                         <div className="flex gap-3 mt-5">
                             <Link className='flex px-4 py-3 rounded-full text-muted border-[0.02rem] border-muted hover:scale-105 transition-all shining-button overflow-hidden relative ' href={'/'}>Our Works</Link>
                             <Link className='flex px-4 py-3 rounded-full bg-gradient-to-t from-primary to-violet-800 hover:scale-105 transition-all shining-button overflow-hidden relative ' href={'/'}>Get a Quote</Link>
                         </div>
+                        <Image src={'/assets/headerimg.png'} width={500} className=" object-top object-cover overflow-hidden" height={200} alt="" />
                     </motion.div>
-                    <div className='absolute bottom-0 z-[1]'>
-                        <Image src={'/assets/headerimg.png'} width={500} className="h-[200px] object-top object-cover overflow-hidden" height={200} alt="" />
-                    </div>
                 </BackgroundGradientAnimation>
             </div>
             <div ref={ref2} className="bg-background min-h-[100vh] w-full overflow-x-hidden">
@@ -69,32 +68,45 @@ const HomePage = () => {
                     </div>
                 </div>
                 <div className='w-full mx-auto mt-10 flex justify-center'>
-                    <div className='max-w-[450px] w-full card rounded-xl justify-center flex flex-col items-center gap-3 p-3'>
+                    <div className='max-w-[450px] px-6 w-full card rounded-xl justify-center flex flex-col items-center gap-3 p-3'>
                         <h2 className='text-[25px] text-violet-400'>Graphic Desiging</h2>
                         <h2>Enhance your brand{'\''}s visual identity with our creative graphic design solutions. From logos to marketing materials, we craft compelling visuals that captivate and engage your audience.</h2>
                         <Link href={'/'} className='border-[0.01rem] rounded-full hover:bg-zinc-50/10 ms-auto px-3 py-2 border-zinc-900'>more.</Link>
                     </div>
                 </div>
             </div>
-            <div ref={ref2} className="bg-background min-h-[100vh] w-full overflow-x-hidden mb-[150px]">
-                <h3 className='title mx-auto text-[50px] font-extrabold w-full text-center mt-10 px-4'>Our Latest Works</h3>
-                <div className='overflow-hidden mt-5'>
-                    <motion.div style={{ marginLeft: `-${hookedYPostion * 600}px` }} className='flex flex-nowrap justify-center gap-5 items-center md:h-[240px] h-[150px] w-max'>
-                        <Image className='workCard' src={'/assets/w2.png'} alt='' height={300} width={400} />
-                        <Image className='workCard' src={'/assets/w1.png'} alt='' height={300} width={400} />
-                        <Image className='workCard' src={'/assets/w3.png'} alt='' height={300} width={400} />
-                        <Image className='workCard' src={'/assets/w4.png'} alt='' height={300} width={400} />
-                        <Image className='workCard' src={'/assets/w5.png'} alt='' height={300} width={400} />
-                    </motion.div>
+            <div ref={ref2} className="bg-background mt-[20px] relative h-[100px] w-full overflow-hidden mb-[150px]">
+                <div className='w-full rotate-45 mt-[-300px]'>
+                    <div className='overflow-hidden mt-5'>
+                        <motion.div style={{ marginLeft: `-${hookedYPostion * 600}px` }} className='flex flex-nowrap justify-center gap-5 items-center md:h-[240px] h-[150px] w-max'>
+                            <Image className='workCard' src={'/assets/w2.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w1.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w3.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w4.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w5.png'} alt='' height={300} width={400} />
+                        </motion.div>
+                    </div>
+                    <div className='overflow-hidden w-full'>
+                        <motion.div style={{ x: `${(hookedYPostion * 600) - 900}px` }} className='flex flex-nowrap justify-center gap-5 items-center md:h-[240px] h-[150px] w-max'>
+                            <Image className='workCard' src={'/assets/w6.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w7.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w8.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w9.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w10.png'} alt='' height={300} width={400} />
+                        </motion.div>
+                    </div>
+                    <div className='overflow-hidden mt-5'>
+                        <motion.div style={{ marginLeft: `-${hookedYPostion * 600}px` }} className='flex flex-nowrap justify-center gap-5 items-center md:h-[240px] h-[150px] w-max'>
+                            <Image className='workCard' src={'/assets/w2.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w1.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w3.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w4.png'} alt='' height={300} width={400} />
+                            <Image className='workCard' src={'/assets/w5.png'} alt='' height={300} width={400} />
+                        </motion.div>
+                    </div>
                 </div>
-                <div className='overflow-hidden w-full'>
-                    <motion.div style={{ x: `${(hookedYPostion * 600) - 900}px` }} className='flex flex-nowrap justify-center gap-5 items-center md:h-[240px] h-[150px] w-max'>
-                        <Image className='workCard' src={'/assets/w6.png'} alt='' height={300} width={400} />
-                        <Image className='workCard' src={'/assets/w7.png'} alt='' height={300} width={400} />
-                        <Image className='workCard' src={'/assets/w8.png'} alt='' height={300} width={400} />
-                        <Image className='workCard' src={'/assets/w9.png'} alt='' height={300} width={400} />
-                        <Image className='workCard' src={'/assets/w10.png'} alt='' height={300} width={400} />
-                    </motion.div>
+                <div className='absolute top-0 bottom-0 flex justify-center items-center left-0 right-0 bg-black/90'>
+                    <h3 className='title mx-auto md:text-[50px] text-[30px] font-extrabold w-full text-center px-4'>Our Latest Works</h3>
                 </div>
             </div>
 
--- components/home/HomeNavbar.tsx
+++ components/home/HomeNavbar.tsx
@@ -12,7 +12,7 @@ const HomeNavbar = () => {
     const [menuopen, setmenuopen] = useState(false);
     const navbarctx = useContext(HomeNavbarContext);
     return (
-        <div className='w-full px-5 pt-4 fixed z-[99999]'>
+        <div className={cn('w-full px-5 pt-4 fixed z-[99999] transition-all duration-500',navbarctx.light ? '':'bg-black')}>
             <nav className='screen p-2 flex justify-between items-center'>
                 <div className='flex items-center gap-4'>
                     <Image src={'/assets/logo.png'} width={50} height={50} alt='' />
@@ -23,7 +23,7 @@ const HomeNavbar = () => {
                 }} className='menu p-2 md:hidden block'>
                     <BiMenuAltRight className={`text-${navbarctx.light ? 'black':'white'}`} size={30} />
                 </div>
-                <div className={cn('md:relative fixed md:right-0 top-0 bottom-0 md:flex flex md:items-center items-end gap-10 md:flex-row flex-col md:justify-end justify-start md:bg-transparent bg-black md:px-0 md:py-0 py-10 transition-all px-[30px] md:text-[16px] text-[24px] md:max-w-[600px] w-full max-w-[350px] ',menuopen ? 'right-0' : 'right-[-350px]')}>
+                <div className={cn('md:relative fixed md:right-0 top-0 bottom-0 md:flex flex md:items-center items-end gap-10 md:flex-row flex-col md:justify-end justify-start md:bg-transparent bg-black md:px-0 md:py-0 py-10 transition-all px-[50px] md:text-[16px] text-[24px] md:max-w-[600px] w-max ',menuopen ? 'right-0' : 'right-[-350px]')}>
                     <div className='md:flex-row md:flex hidden flex-col items-center gap-10 '>
                         <StaggerHoverText color={navbarctx.light ? 'black':'white'} link='/' text='Home' />
                         <StaggerHoverText color={navbarctx.light ? 'black':'white'} link='/' text='Pricing' />
@@ -37,6 +37,7 @@ const HomeNavbar = () => {
                             Close <X className='' size={30} />
                         </div>
                         <Link href={'/'}>Home</Link>
+                        <Link href={'/'}>Pricings</Link>
                         <Link href={'/'}>Our Works</Link>
                         <Link href={'/'}>Contact</Link>
                     </div>
