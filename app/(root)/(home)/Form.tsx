0176c7694a685a8ab39f577090e8cf2a76240373
Abin Antony <abina5448@gmail.com>
2024-05-21 12:33:03 +0530

    form update

--- app/(root)/(home)/Form.tsx
+++ app/(root)/(home)/Form.tsx
@@ -1,7 +1,8 @@
 'use client'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const Form = () => {
     const [name, setname] = useState('');
@@ -9,45 +10,60 @@ const Form = () => {
     const [mail, setmail] = useState('');
     const [type, settype] = useState('');
     const [message, setmessage] = useState('');
-    const [error,setError ] = useState('');
-        const [loading,setLoading ] = useState(false);
-    
-    const handleSubmit = () =>{
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [loading, setLoading] = useState(false);
+    const form = useRef<HTMLFormElement>(null);
+    const validation = () =>{
         setError('');
-        if(name == ''){
+        if (name == '') {
             setError('Please enter name');
         }
-        if(phone == ''){
+        if (phone == '') {
             setError('Please enter phone');
         }
-        if(mail == ''){
+        if (mail == '') {
             setError('Please enter mail');
         }
-        if(type == ''){
+        if (type == '') {
             setError('Please select service');
         }
-        if(message == ''){
+        if (message == '') {
             setError('Please enter your message');
         }
-        if(error == ''){
+        return error;
+    }
+    const handleSubmit = async () => {
+        var e = validation();
+        if (e == '') {
             setLoading(true);
-            let formData:FormData = new FormData();
-            formData.append('name',name);
-            formData.append('phone',phone);
-            formData.append('mail',mail);
-            formData.append('type',type);
-            formData.append('message',message);
-            fetch('https://porukaracollege.in/api/webcode/form',{
-                method:'post',
-                body:JSON.stringify({
-                    name:name,
-                    phone:phone,
-                    mail:mail,
-                    type:type,
-                    message:message
+            let formData: FormData = new FormData();
+            formData.append('name', name);
+            formData.append('phone', phone);
+            formData.append('mail', mail);
+            formData.append('type', type);
+            formData.append('message', message);
+            await fetch('https://porukaracollege.in/api/webcode/form', {
+                method: 'post',
+                body: JSON.stringify({
+                    name: name,
+                    phone: phone,
+                    mail: mail,
+                    type: type,
+                    message: message
                 }),
-            }).then((res)=>res.json()).then((json)=>console.log(json));
+            }).then((res) => res.json()).then((json) => {
+                if (json.error == null) {
+                    setSuccess('Your message send Successfully');
+                    setTimeout(()=>{
+                        setSuccess('');
+                    },4000)
+                }
+                form.current?.reset();
+                setLoading(false);
+            });
         }
+
     }
     return (
         <div className='screen py-10 px-3 flex ' id='Contact'>
@@ -57,16 +73,22 @@ const Form = () => {
 
 
                 </div> */}
-                <form className='flex flex-col gap-3 px-5 py-4 max-w-[600px] mx-auto w-full'>
+                <form ref={form} className='flex flex-col gap-3 px-5 py-4 max-w-[600px] mx-auto w-full'>
                     <h3 className='title md:text-[50px] text-[30px] font-extrabold '>Contact Us</h3>
                     <p className='text-zinc-400 text-[13px]'>Fill this form and share your ideas with us!</p>
+                    {error != '' && <div className='bg-red-800 text-white px-4 py-3 rounded-md mt-2 mb-3'>
+                        {error}
+                    </div>}
+                    {success != '' && <div className='bg-green-800 text-white px-4 py-3 rounded-md mt-2 mb-3'>
+                        {success}
+                    </div>}
                     <div className='flex md:flex-row flex-col gap-4 max-w-[800px] w-full'>
-                        <input name='name' value={name} onChange={(e)=>setname(e.currentTarget.value)} className='bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none' placeholder='Name' />
-                        <input name='mail' value={mail} onChange={(e)=>setmail(e.currentTarget.value)} className='bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none' placeholder='  Email' />
+                        <input name='name' value={name} onChange={(e) => setname(e.currentTarget.value)} className='bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none' placeholder='Name' />
+                        <input name='mail' value={mail} onChange={(e) => setmail(e.currentTarget.value)} className='bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none' placeholder='  Email' />
                     </div>
                     <div className='flex md:flex-row flex-col gap-4 max-w-[800px] w-full'>
-                        <input name='phone' value={phone} onChange={(e)=>setphone(e.currentTarget.value)} className='bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none' placeholder='  Phone (including country code)' />
-                        <select value={type} onChange={(e)=>settype(e.currentTarget.value)} className='bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none'>
+                        <input name='phone' value={phone} onChange={(e) => setphone(e.currentTarget.value)} className='bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none' placeholder='  Phone (including country code)' />
+                        <select value={type} onChange={(e) => settype(e.currentTarget.value)} className='bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none'>
                             <option value={0}>-- select needed service --</option>
                             <option value={'Web Designing'}>Web Designing</option>
                             <option value={'Web Development'}>Web Development</option>
@@ -75,9 +97,9 @@ const Form = () => {
                             <option value={'Logo Designing'}>Logo Designing</option>
                         </select>
                     </div>
-                    <textarea value={message} onChange={(e)=>setmessage(e.currentTarget.value)} className='min-h-[100px] bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none' placeholder='  message'>
+                    <textarea value={message} onChange={(e) => setmessage(e.currentTarget.value)} className='min-h-[100px] bg-black px-4 py-4 rounded-xl w-full border-zinc-900 transition-all border-[0.01rem] focus-visible:border-zinc-200 outline-none' placeholder='  message'>
                     </textarea>
-                    <button className='flex self-end px-6 py-3 rounded-full bg-gradient-to-t from-primary to-violet-800 hover:scale-105 transition-all shining-button overflow-hidden relative' onClick={()=>handleSubmit()} type='button' >Submit</button>
+                    <button className={cn('flex self-end px-6 py-3 rounded-full bg-gradient-to-t  hover:scale-105 transition-all shining-button overflow-hidden relative', loading ? 'from-secondary to-zinc-600' : 'from-primary to-violet-800')} onClick={() => handleSubmit()} type='button' >{loading ? "Submitting" : 'Submit'}</button>
                 </form>
             </div>
         </div>
